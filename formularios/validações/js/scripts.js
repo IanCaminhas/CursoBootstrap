b42f41f42e6a366b55ebba91dfc5779f2cad139d
IanPC <iancaminhas25@hotmail.com>
2024-01-22 16:56:35 -0300

    Termino formularios

---
+++ formularios/validações/js/scripts.js
@@ -0,0 +1,20 @@
+//Objetivo: impossibitar o envio dos dados com erro para o servidor
+//Essa function auto se invoca
+
+(function () {
+    'use strict'
+  
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
